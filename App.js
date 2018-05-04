802b352603ee0424e5a71605f2a2fa4fb2857084
Edison Alulema <edisonalulema@Edisons-MacBook-Air.local>
2018-05-04 12:29:17 -0400

    Location update

--- App.js
+++ App.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import { Constants, Location, Permissions } from 'expo';
+import { MapView } from 'expo';
 
-export default class App extends React.Component {
+
+export default class App extends Component {
+  state = {
+    location: null,
+    errorMessage: null,
+	};
+
+	componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+	};
+
+	getLocationAsync = async () => {
+		const { Location, Permissions } = Expo;
+		const { status } = await Permissions.askAsync(Permissions.LOCATION);
+		if (status === 'granted') {
+			return Location.getCurrentPositionAsync({enableHighAccuracy: true});
+		} else {
+			throw new Error('Location permission not granted');
+		}
+	}
+
 	render() {
+		let text = 'Waiting..';
+    if (this.state.errorMessage) {
+      text = this.state.errorMessage;
+    } else if (this.state.location) {
+      text = JSON.stringify(this.state.location);
+		}
+
 		return (
+
 			<View style={styles.buttons}>
 				<FontAwesome.Button
 					name="facebook"
