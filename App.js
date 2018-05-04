132c35b2059caee822b7ad50f084eb00af70cfde
Jeton Lolovic <jetlol79@gmail.com>
2018-05-04 15:03:55 -0400

    theres communication with firebase

--- App.js
+++ App.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+var config = {
+	apiKey: 'AIzaSyDEJFDnT7LQJE6K1zn3uw_eM2ELHS-n-Yk',
+	authDomain: 'parkupied.firebaseapp.com',
+	databaseURL: 'https://parkupied.firebaseio.com',
+	projectId: 'parkupied',
+	storageBucket: 'parkupied.appspot.com',
+	messagingSenderId: '641475186680'
+};
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+
+const settings = {
+	timestampsInSnapshots: true
+};
+firestore.settings(settings);
+
 
 export default class App extends React.Component {
+
+
+	componentDidMount() {
+		firestore.collection('users').doc('L0WhdtLGqO8sv1IipMbn').get()
+			.then(users => {
+				console.log('USERSSS', users.data());
+			})
+		firestore.collection('users').doc('L0WhdtLGqO8sv1IipMbn').set({
+			carInfo: { color: 'white'}
+		})
+	}
+
 	render() {
 		return (
 			<View style={styles.buttons}>
@@ -18,7 +51,7 @@ export default class App extends React.Component {
 					backgroundColor="#DD4B39"
 					{...iconStyles}
 				>
-					Or with Google
+					Login with Google
 				</FontAwesome.Button>
 			</View>
 		);
