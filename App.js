ce8d3028f815f771042722314e52e3f10b5eeddd
Jeton Lolovic <jetlol79@gmail.com>
2018-05-03 23:42:14 -0400

    added Login with Facebook and Google buttons

--- App.js
+++ App.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up Parkupied to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.buttons}>
+				<FontAwesome.Button
+					name="facebook"
+					backgroundColor="#3b5998"
+					{...iconStyles}
+				>
+					Login with Facebook
+				</FontAwesome.Button>
+				<FontAwesome.Button
+					name="google"
+					backgroundColor="#DD4B39"
+					{...iconStyles}
+				>
+					Or with Google
+				</FontAwesome.Button>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		color: 'blue',
+		fontSize: 35,
+		borderColor: 'black',
+	},
+	buttons: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		margin: 20,
+		marginTop: 'auto'
+	},
 });
+
+const iconStyles = {
+	borderRadius: 10,
+	iconStyle: { paddingVertical: 5 },
+};
