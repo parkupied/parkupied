34da4f69b8731c89ec2083033a8b446a9c586bd8
Oleksii Borysonok <alex.borysonok@gmail.com>
2018-05-08 12:02:23 -0400

    put all markers on the map

--- components/Map.js
+++ components/Map.js
@@ -7,35 +7,71 @@ import firestore from '../firestore';
 
 
 export default class Map extends Component {
-constructor() {
-  super();
-  this.state = {
-    location: null,
-    errorMessage: null,
+  constructor() {
+    super();
+    this.state = {
+      location: null,
+      errorMessage: null,
+      markers: [],
+      userPos: {}
+    }
+    this.handleGive = this.handleGive.bind(this);
+    this.handleLook = this.handleLook.bind(this);
   };
-  this.handleGive = this.handleGive.bind(this);
-  this.handleLook = this.handleLook.bind(this);
 
-
-}
-componentWillMount() {
-  this._getLocationAsync();
-}
-
-handleLook(event) {
-  console.log("Looking");
-}
-
-handleGive(event) {
-  console.log("Giving");
-  firestore.collection('parkingSpots').add({
-    Coordinates: this.state.location
+  componentWillMount() {
+    this._getLocationAsync();
   }
-  );
-}
 
+  handleLook(event) {
+    console.log("Looking");
+  }
 
+  //   id: 2,
+  //   latlng: { latitude: 40.70486398294511, longitude: -74.0088782067479 },
+  //   title: 'marker-green',
+  //   description: 'Now you can go...MOVE!!!'
 
+  // Object {
+  //   11:55:30:   "Coordinates": Object {
+  //   11:55:30:     "coords": Object {
+  //   11:55:30:       "accuracy": 20,
+  //   11:55:30:       "altitude": 0,
+  //   11:55:30:       "heading": 0,
+  //   11:55:30:       "latitude": 40.7456107,
+  //   11:55:30:       "longitude": -73.8703107,
+  //   11:55:30:       "speed": 0,
+  //   11:55:30:     },
+  //   11:55:30:     "mocked": false,
+  //   11:55:30:     "timestamp": 1525754668227,
+  //   11:55:30:   },
+  //   11:55:30: }
+
+  handleGive(event) {
+    console.log("Giving");
+    firestore.collection('parkingSpots')
+      .add({ Coordinates: this.state.location })
+      .then(() => firestore.collection('parkingSpots').get())
+      .then(allSpots => {
+        let tempMarkers = [];
+        allSpots.forEach(spot => {
+          console.log(spot.data().Coordinates.coords);
+          const obj = spot.data().Coordinates.coords;
+          const latlng = {latitude:obj.latitude, longitude: longitude }
+          // create a new marker based on data from DB:
+          //   let newMarker = {
+          //     id: spot.id,
+          //     latlng: spot.coords,
+          //     title: 'some fake title....',
+          //     description: 'some fake description....'
+          //   }
+          //   // then put this new marker into the temporary array:
+          //   tempMarkers.push(newMarker);
+          // });
+          // this.setState({ markers: tempMarkers });
+        })
+      })
+  }
 
   _getLocationAsync = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
@@ -47,20 +83,20 @@ handleGive(event) {
 
     let location = await Location.getCurrentPositionAsync({});
     this.setState({ location, ready: true });
-	};
+  };
 
-	getLocationAsync = async () => {
-		const { Location, Permissions } = Expo;
-		const { status } = await Permissions.askAsync(Permissions.LOCATION);
-		if (status === 'granted') {
-			return Location.getCurrentPositionAsync({enableHighAccuracy: true});
-		} else {
-			throw new Error('Location permission not granted');
-		}
-	}
+  getLocationAsync = async () => {
+    const { Location, Permissions } = Expo;
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status === 'granted') {
+      return Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  }
 
   onRegionChangeComplete = (location) => {
-    console.log('onRegionChangeComplete', location);
+   // console.log('onRegionChangeComplete', location);
   };
 
   setRegion(location) {
@@ -69,7 +105,7 @@ handleGive(event) {
     }
   }
 
-	render() {
+  render() {
 
     // console.log("LOCATION", this.state.location);
 
@@ -86,33 +122,41 @@ handleGive(event) {
 
     return (
       <View style={styles.container}>
-      <MapView
-      style={styles.map}
-      showsUserLocation={true}
-      followsUserLocation={true}
-        initialregion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        onRegionChangeComplete={this.onRegionChangeComplete}
-      />
-      <Button
-      title="Give up Parking!"
-      onPress={this.handleGive}
-      >
-      Give up Parking!
-      </Button>
-      <Button
-      title="Look For Parking!"
-      onPress={this.handleLook}
-      >
-      Look for Parking!
-      </Button>
-        </View>
-		);
-	}
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          followsUserLocation={true}
+          initialregion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onRegionChangeComplete={this.onRegionChangeComplete}>
+
+          {this.state.markers.map(marker => (
+            <Marker
+              key={marker.id}
+              coordinate={marker.latlng}
+              title={marker.title}
+              description={marker.description} />
+          ))}
+
+        </MapView>
+
+        <Button
+          title="Give up Parking!"
+          onPress={this.handleGive}>
+          Give up Parking!
+        </Button>
+        <Button
+          title="Look For Parking!"
+          onPress={this.handleLook}>
+          Look for Parking!
+        </Button>
+      </View>
+    );
+  }
 }
 
 
